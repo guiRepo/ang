a9bd9958d8e575560a8aa6c8db10947aa2949d05
Guilherme Andre <guilhermeanm95@htomail.com>
2022-11-23 23:17:39 -0300

    emit parent and child

--- src/app/components/emit-child/emit-child.component.ts
+++ src/app/components/emit-child/emit-child.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-emit-child',
   templateUrl: './emit-child.component.html',
-  styleUrls: ['./emit-child.component.css']
+  styleUrls: ['./emit-child.component.css'],
 })
 export class EmitChildComponent {
-
+  @Output() changeNumber: EventEmitter<any> = new EventEmitter();
+  handleClick() {
+    console.log('mudou o numero');
+    this.changeNumber.emit();
+  }
 }
--- src/app/components/emit-parent/emit-parent.component.ts
+++ src/app/components/emit-parent/emit-parent.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-emit-parent',
   templateUrl: './emit-parent.component.html',
-  styleUrls: ['./emit-parent.component.css']
+  styleUrls: ['./emit-parent.component.css'],
 })
 export class EmitParentComponent {
-
+  randomNun: number = 12;
+  onChangeNumber() {
+    this.randomNun = Math.floor(Math.random() * 10);
+  }
 }
