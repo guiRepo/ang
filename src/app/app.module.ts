d6a19058cd47d8d6e3c8a3e65bfc69777a8f96a9
Guilherme Andre <guilhermeanm95@htomail.com>
2022-11-23 22:30:59 -0300

    if rednder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FisrtComponentComponent } from './components/fisrt-component/fisrt-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FisrtComponentComponent,
     ParentDataComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule
